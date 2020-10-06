4f450970f360c44740ba9d362a3f3079f261e3c7
Rory Murray <21082125+rorychristianmurray@users.noreply.github.com>
2020-10-06 12:41:07 -0400

    Uses Tellor API to fetch ETH, BTC, and AMPL price feed data from node endpoint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const account = require('./account');
 const path = require("path");
+const axios = require('axios')
 const app = express();
 
 app.use( bodyParser.json() );  // to support JSON-encoded bodies
@@ -35,6 +36,36 @@ app.get('/api/test', (req, res, next) => {
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+// uses Tellor HTTP API to get latest prices
+app.get("/assetPrices", (req, res) => {
+  let priceArray = []
+  // GET ETH/USD price
+  axios.get('http://api.tellorscan.com/price/1')
+  .then(getResponse => {
+    priceArray.push(getResponse.data.value)
+  })
+
+  .then(() => {
+    // GET BTC/USD price
+    axios.get('http://api.tellorscan.com/price/2')
+    .then(getResponse2 => {
+      priceArray.push(getResponse2.data.value)
+    })
+    
+    .then(() => {
+      // GET AMPL/USD price
+      axios.get('http://api.tellorscan.com/price/10')
+      .then(getResponse3 => {
+        priceArray.push(getResponse3.data.value)
+      })
+
+      .then(() => {
+        res.json({'prices': priceArray})
+      })
+    })
+  })
 });
 
 const port = process.env.PORT || 8080;
